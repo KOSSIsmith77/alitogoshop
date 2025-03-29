904aa602a883ffdd3ee044aafd735906ee0636d6
KOSSIsmith77 <attissosmith2@gmail.com>
2025-03-29 18:09:05 +0100

    Add files via upload

--- singup.js
+++ singup.js
@@ -1,3 +1,7 @@
 // Lors de l'inscription
 const phone = document.getElementById('phone').value;
 localStorage.setItem('vendorPhone', phone);
+function traduire(langue) {
+    let traductions = { "fr": "Bienvenue", "en": "Welcome", "es": "Bienvenido" };
+    document.getElementById("titre").textContent = traductions[langue] || "Bienvenue";
+}
